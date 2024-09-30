bd174ad0fb4a3f32f66b550da815cc9ad9ded3da
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-09-30 12:34:47 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,8 +7,10 @@ public class Main {
       String scroll2 = "Beware of the dragon's lair!";
       String secretMessage = "The treasure lies beyond the mountains.";
       
-      System.out.println("Scroll 1: " + scroll1.length() + " characters.");
-      System.out.println("Scroll 2: " + scroll2.length() + " characters.");
+      System.out.println("Scroll 1: " + scroll1);
+      System.out.println(scroll1.length());
+      System.out.println("Scroll 2: " + scroll2);
+      System.out.println(scroll2.length());
 
       System.out.println("Key phrase from Scroll 1: " + scroll1.substring(16, 32)); 
       System.out.println("Hidden warning in Scroll 2: " + scroll2.substring(12));
@@ -16,14 +18,14 @@ public class Main {
       System.out.println("Position of 'treasure' in Scroll 1: " + scroll1.indexOf("treasure"));
       System.out.println("Position of 'dragon' in Scroll 1: " + scroll1.indexOf("dragon"));
 
-      System.out.println("Do Scroll 1 and the secret message match?: " + scroll1.equals(secretMessage));
-      System.out.println("Do Scroll 2 and the secret message match?: " + scroll2.equals(secretMessage));
+      System.out.println("Does Scroll 1 match the secret message?: " + scroll1.equals(secretMessage));
+      System.out.println("Does Scroll 2 match the secret message?: " + scroll2.equals(secretMessage));
 
-      System.out.println("Result of comparing Scroll 1 to Scroll 2: " + scroll1.compareTo(scroll2));
-      System.out.println("Result of comparing Scroll 2 to Scroll 1: " + scroll2.compareTo(scroll1));
+      System.out.println("Comparing Scroll 1 to Scroll 2: " + scroll1.compareTo(scroll2));
+      System.out.println("Comparing Scroll 2 to Scroll 1: " + scroll2.compareTo(scroll1));
 
       // *** MATH CLASS METHODS ***
-
+   
 
    }
 }
